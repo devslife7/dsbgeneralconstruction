6afcb77d70e84f81575685ab0bde488f4851f72b
Marcos Velasco <devslife7@gmail.com>
2023-12-04 18:23:26 -0500

    Changed file casing

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import AboutSection from "@/components/home/AboutSection"
 import Hero from "@/components/home/Hero"
 import Services from "@/components/home/Services"
 import ContactSection from "@/components/home/ContactSection"
-import KeyPoints from "@/components/home/KeyPoints"
+// import KeyPoints from "@/components/home/KeyPoints"
+import Points from "@/components/home/Points"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutSection />
-      <KeyPoints />
+      {/* <KeyPoints /> */}
+      <Points />
       <Services />
       <ContactSection className="lg:mb-36 lg:mt-40" />
     </>
