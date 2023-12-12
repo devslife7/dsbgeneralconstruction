1c07f8e3999c120e9492d9a3bcdf095fd94bdd24
Marcos Velasco <devslife7@gmail.com>
2023-12-12 12:17:32 -0500

    get gallery

--- components/work/gallery.tsx
+++ components/work/gallery.tsx
@@ -1,8 +1,8 @@
 // import GalleryCard from "./galleryCard/galleryCard"
-// import { getWorkList } from "@/actions/work"
+import { getWorkList } from "@/actions/work"
 
 export default async function Gallery() {
-  // const gallery = await getWorkList()
+  const gallery = await getWorkList()
   return (
     <div className="mx-auto lg:container lg:mt-10 lg:px-8">
       <div className="my-container">
