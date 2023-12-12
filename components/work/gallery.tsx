4519fcc09671773868a59d6bfb5f49744e4b584a
Marcos Velasco <devslife7@gmail.com>
2023-12-12 12:15:24 -0500

    introduciont more logic

--- components/work/gallery.tsx
+++ components/work/gallery.tsx
@@ -4,20 +4,19 @@
 export default async function Gallery() {
   // const gallery = await getWorkList()
   return (
-    <div>gallery</div>
-    // <div className="mx-auto lg:container lg:mt-10 lg:px-8">
-    // {/* <div className="my-container">
-    //   <h1 className="text-center text-4xl font-bold opacity-70 mb-10 lg:mb-1 hidden lg:block">Our Work</h1>
-    //   <p className="hidden lg:block text-center w-full m-auto opacity-60 mb-10">
-    //     You&#39;re invited to browse a selection of recently completed projects below.
-    //   </p>
-    // </div>
+    <div className="mx-auto lg:container lg:mt-10 lg:px-8">
+      <div className="my-container">
+        <h1 className="mb-10 hidden text-center text-4xl font-bold opacity-70 lg:mb-1 lg:block">Our Work</h1>
+        <p className="m-auto mb-10 hidden w-full text-center opacity-60 lg:block">
+          You&#39;re invited to browse a selection of recently completed projects below.
+        </p>
+      </div>
 
-    // <div className="flex flex-wrap justify-center gap-20 lg:gap-10 lg:justify-start">
-    //   {gallery.map((work: any, index: number) => (
-    //     <GalleryCard key={index} work={work} />
-    //   ))}
-    // </div> */}
-    // </div>
+      {/* <div className="flex flex-wrap justify-center gap-20 lg:gap-10 lg:justify-start">
+      {gallery.map((work: any, index: number) => (
+        <GalleryCard key={index} work={work} />
+      ))}
+    </div> */}
+    </div>
   )
 }
