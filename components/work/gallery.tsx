6c770b0c5799e6f9d0d617744713932d519ba31b
Marcos Velasco <devslife7@gmail.com>
2023-12-12 12:13:27 -0500

    removed more logic

--- components/work/gallery.tsx
+++ components/work/gallery.tsx
@@ -1,22 +1,23 @@
-import GalleryCard from "./galleryCard/galleryCard"
-import { getWorkList } from "@/actions/work"
+// import GalleryCard from "./galleryCard/galleryCard"
+// import { getWorkList } from "@/actions/work"
 
 export default async function Gallery() {
-  const gallery = await getWorkList()
+  // const gallery = await getWorkList()
   return (
-    <div className="mx-auto lg:container lg:mt-10 lg:px-8">
-      {/* <div className="my-container">
-        <h1 className="text-center text-4xl font-bold opacity-70 mb-10 lg:mb-1 hidden lg:block">Our Work</h1>
-        <p className="hidden lg:block text-center w-full m-auto opacity-60 mb-10">
-          You&#39;re invited to browse a selection of recently completed projects below.
-        </p>
-      </div>
+    <div>gallery</div>
+    // <div className="mx-auto lg:container lg:mt-10 lg:px-8">
+    // {/* <div className="my-container">
+    //   <h1 className="text-center text-4xl font-bold opacity-70 mb-10 lg:mb-1 hidden lg:block">Our Work</h1>
+    //   <p className="hidden lg:block text-center w-full m-auto opacity-60 mb-10">
+    //     You&#39;re invited to browse a selection of recently completed projects below.
+    //   </p>
+    // </div>
 
-      <div className="flex flex-wrap justify-center gap-20 lg:gap-10 lg:justify-start">
-        {gallery.map((work: any, index: number) => (
-          <GalleryCard key={index} work={work} />
-        ))}
-      </div> */}
-    </div>
+    // <div className="flex flex-wrap justify-center gap-20 lg:gap-10 lg:justify-start">
+    //   {gallery.map((work: any, index: number) => (
+    //     <GalleryCard key={index} work={work} />
+    //   ))}
+    // </div> */}
+    // </div>
   )
 }
