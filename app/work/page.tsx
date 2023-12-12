eb99841ef0cb7dc0306b8d4f0c892b20a06f7f8d
Marcos Velasco <devslife7@gmail.com>
2023-12-12 12:07:38 -0500

    put back gallery

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,18 +1,14 @@
 import { Suspense } from "react"
 import Gallery from "@/components/work/gallery"
-import AddWorkInterface from "@/components/work/addWorkInterface"
+// import AddWorkInterface from "@/components/work/addWorkInterface"
 
 export default function page() {
   return (
     <>
       {/* <AddWorkInterface /> */}
-      {/* <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <Gallery />
-      </Suspense> */}
-      <div>here</div>
-      <div>here</div>
-      <div>here</div>
-      <div>here</div>
+      </Suspense>
     </>
   )
 }
