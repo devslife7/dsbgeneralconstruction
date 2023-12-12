cc406477772fecf4cf85a38c7205bba2bb1467e6
Marcos Velasco <devslife7@gmail.com>
2023-12-12 12:11:10 -0500

    norrowing down

--- components/work/gallery.tsx
+++ components/work/gallery.tsx
@@ -4,8 +4,8 @@ import { getWorkList } from "@/actions/work"
 export default async function Gallery() {
   const gallery = await getWorkList()
   return (
-    <div className="lg:mt-10 mx-auto lg:container lg:px-8">
-      <div className="my-container">
+    <div className="mx-auto lg:container lg:mt-10 lg:px-8">
+      {/* <div className="my-container">
         <h1 className="text-center text-4xl font-bold opacity-70 mb-10 lg:mb-1 hidden lg:block">Our Work</h1>
         <p className="hidden lg:block text-center w-full m-auto opacity-60 mb-10">
           You&#39;re invited to browse a selection of recently completed projects below.
@@ -16,7 +16,7 @@ export default async function Gallery() {
         {gallery.map((work: any, index: number) => (
           <GalleryCard key={index} work={work} />
         ))}
-      </div>
+      </div> */}
     </div>
   )
 }
