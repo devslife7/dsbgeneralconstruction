15fae5b7d6b6f3e627171b5facf552b76e85c3ff
Marcos Velasco <devslife7@gmail.com>
2023-12-12 22:33:30 -0500

    enabled addworkinter

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,11 +1,11 @@
 // import { Suspense } from "react"
 import Gallery from "@/components/work/gallery"
-// import AddWorkInterface from "@/components/work/addWorkInterface"
+import AddWorkInterface from "@/components/work/addWorkInterface"
 
 export default function page() {
   return (
     <>
-      {/* <AddWorkInterface /> */}
+      <AddWorkInterface />
       {/* <Suspense fallback={<div>Loading...</div>}> */}
       <Gallery />
       {/* </Suspense> */}
