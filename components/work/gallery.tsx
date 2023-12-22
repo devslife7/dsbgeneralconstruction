e90fd06405e4a6040c386542fc4f9331eb4f4149
Marcos Velasco <devslife7@gmail.com>
2023-12-22 03:19:15 -0500

    added margin botton here

--- components/work/gallery.tsx
+++ components/work/gallery.tsx
@@ -4,7 +4,7 @@ import { getWorkList } from "@/actions/work"
 export default async function Gallery() {
   const gallery = await getWorkList()
   return (
-    <div className="mx-auto lg:container lg:mt-10 lg:px-8">
+    <div className="mx-auto mb-10 lg:container lg:mt-10 lg:px-8">
       <div className="my-container">
         <h1 className="mb-10 hidden text-center text-4xl font-bold opacity-70 lg:mb-1 lg:block">Our Work</h1>
         <p className="m-auto mb-10 hidden w-full text-center opacity-60 lg:block">
