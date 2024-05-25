0283dca5f42ff42620aa2b6c10b76f29d0b6e161
Marcos Velasco <devslife7@gmail.com>
2024-05-25 17:50:33 -0400

    added some animation to gallery thumgnails

--- components/work/image-gallery.tsx
+++ components/work/image-gallery.tsx
@@ -1,19 +1,26 @@
 "use client"
 // Import Swiper React components
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules"
+import { Navigation, Pagination, FreeMode, Thumbs } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
 import Image from "next/image"
 
 // Import Swiper styles
 import "swiper/css"
-import "swiper/css/navigation"
+import "swiper/css/bundle"
 import "swiper/css/pagination"
-import "swiper/css/scrollbar"
+import "swiper/css/free-mode"
+import "swiper/css/navigation"
+import "swiper/css/thumbs"
+import React, { useRef, useState } from "react"
 
 export default function ImageGallery({ gallery }: { gallery: any }) {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null)
+
+  console.log("thumbsSwiper", thumbsSwiper)
+
   const renderSlides = () => {
     return gallery.map((url: any, idx: number) => (
-      <SwiperSlide key={idx}>
+      <SwiperSlide key={idx} aria-activedescendant="opacity-0">
         <Image
           src={url}
           alt={`Image ${idx + 1}`}
@@ -26,18 +33,36 @@ export default function ImageGallery({ gallery }: { gallery: any }) {
     ))
   }
   return (
-    <Swiper
-      style={
-        {
-          "--swiper-navigation-color": "#fff",
-          color: "#fff"
-        } as React.CSSProperties
-      }
-      navigation
-      pagination={{ clickable: true, type: "fraction" }}
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-    >
-      {renderSlides()}
-    </Swiper>
+    <>
+      <Swiper
+        className="mb-2 rounded-lg"
+        loop
+        style={
+          {
+            "--swiper-navigation-color": "#fff",
+            color: "#fff"
+          } as React.CSSProperties
+        }
+        navigation
+        pagination={{ clickable: true, type: "fraction" }}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs, Pagination]}
+      >
+        {renderSlides()}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        slideActiveClass="bg-primary"
+        loop
+        freeMode
+        watchSlidesProgress
+        spaceBetween={10}
+        slidesPerView={4}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper rounded-lg transition-all duration-300 ease-in-out"
+      >
+        {renderSlides()}
+      </Swiper>
+    </>
   )
 }
