993b4f47b9ebbc02634872cb5b17c1a58525589e
Marcos Velasco <devslife7@gmail.com>
2024-08-19 23:59:13 -0400

    added testimonials component

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import Hero from "@/components/home/Hero"
 import Services from "@/components/home/Services"
 import ContactSection from "@/components/home/ContactSection"
 import Points from "@/components/home/Points"
+import Testimonials from "@/components/home/Testimonials"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutSection />
-      <Points />
+      {/* <Points /> */}
+      <Testimonials />
+
       <Services />
       <ContactSection className="lg:mb-36 lg:mt-40" />
     </>
